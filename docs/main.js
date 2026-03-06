74b271f4d12d21f9e75add9b0c22b2ea879967d0
evanblokender <157309371+evanblokender@users.noreply.github.com>
2026-03-06 17:36:28 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -327,7 +327,7 @@ let currentInfiniteSong = null;
 let encryptedAnswer = "";
 let actualAnswer = ""; 
 
-const APP_VERSION = "6.0.3";
+const APP_VERSION = "6.0.4";
 
 const audio = document.getElementById("audio");
 const playBtn = document.getElementById("play-btn");
